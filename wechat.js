5b9c6acfc321d00eabaf4ae9f5d5234f74397947
George Chung <Gerhut@GMail.com>
2014-08-04 17:07:20 +0800

    zoom gif & add send to friend

--- wechat.js
+++ wechat.js
@@ -1,11 +1,31 @@
+void function () {
+
+var data = {
+  imageURL: location.href + 'icon.gif',
+  imageSize: 30,
+  link: location.href,
+  getText: function () { return '来试试老程做的贪吃蛇呵呵呵' }
+}
+
 document.addEventListener('WeixinJSBridgeReady', function () {
+  WeixinJSBridge.on('menu:share:appmessage', function() {
+    WeixinJSBridge.invoke('sendAppMessage', {
+      img_url: data.imageURL,
+      img_width: data.imageSize,
+      img_height: data.imageSize,
+      link: data.link,
+      title: data.getText(),
+      desc: data.getText()
+    })
+  })
   WeixinJSBridge.on('menu:share:timeline', function () {
     WeixinJSBridge.invoke('shareTimeline', {
-      img_url: location.href + 'icon.gif',
-      img_width: 200, img_height: 300,
-      link: location.href,
-      title: '来试试老程做的贪吃蛇呵呵呵',
-      desc: '来试试老程做的贪吃蛇呵呵呵'
-    });
-  });
-});
+      img_url: data.imageURL,
+      img_width: data.imageSize,
+      img_height: data.imageSize,
+      link: data.link,
+      title: data.getText()
+    })
+  })
+})
+} ()
